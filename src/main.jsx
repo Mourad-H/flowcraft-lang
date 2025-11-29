f39ecc73518ab90355539a6fa2d4ea6c892f4211
Eldamon <damonmoreau4@gmail.com>
2025-11-29 12:13:39 +0100

    Fix CSS pipeline and restore main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' 
+import './index.css' // 🛑 هذا السطر يضمن تحميل التصميم 🛑
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
