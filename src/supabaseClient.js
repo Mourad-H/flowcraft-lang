41ca7782c03c018312389e4b622194500743dfbd
Mourad-H <68462669+Mourad-H@users.noreply.github.com>
2025-11-30 16:43:29 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -6,7 +6,8 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true, // إجبار الحفظ
-    autoRefreshToken: true,
-    detectSessionInUrl: true
+    storage: window.localStorage, // استخدام التخزين المحلي الصريح (الأكثر ثباتاً)
+    autoRefreshToken: true, // تجديد الرمز تلقائياً
+    detectSessionInUrl: true // التقاط الرابط عند العودة من الإيميل
   }
 })
